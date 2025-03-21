b88a9f56a8428434ae88fb5a0df6a2405247f723
paras chhugani <51089028+paraschhugani@users.noreply.github.com>
2025-03-21 16:34:20 +0530

    deplyment push

--- services/ai-model-service.ts
+++ services/ai-model-service.ts
@@ -76,6 +76,7 @@ export class AIModelService {
 
   async getVoiceModelList(token : string) {
       try{
+        
         const response = await axios.get(`${this.baseURL}/get-voice-list`, {
           headers: {
             Authorization: `Bearer ${token}`,
