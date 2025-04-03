6dff2b792b2b05356fb03204055a62548c2bef10
paras chhugani <51089028+paraschhugani@users.noreply.github.com>
2025-04-03 19:20:08 +0530

    paddding added to the continue to integration

--- components/script-editor/script-editor.tsx
+++ components/script-editor/script-editor.tsx
@@ -405,7 +405,7 @@ export const ScriptEditor = forwardRef<{ handleSave: () => void}, ScriptEditorPr
         </TabsContent>
       </Tabs>
 
-      <div className={`flex justify-between p-6 border-t md:flex-row flex-col ${!dashboard ? "fixed bottom-0 left-0 w-full bg-white shadow-[0_-4px_10px_rgba(0,0,0,0.1)]" : ""}`}>
+      <div className={`flex justify-between p-6 border-t md:flex-row flex-col ${!dashboard ? "fixed bottom-0 left-0 w-full px-56 bg-white shadow-[0_-4px_10px_rgba(0,0,0,0.1)]" : ""}`}>
         <Button onClick={() => ref && typeof ref === 'object' && ref.current?.handleSave()} variant="outline">
           <Save className="mr-2 h-4 w-4" />
           Save Changes
