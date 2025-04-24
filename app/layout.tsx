efffd3be392c61732d340299a6b24741e52dd190
paras chhugani <51089028+paraschhugani@users.noreply.github.com>
2025-04-24 22:02:46 +0530

    clarity ID update

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "qvbjhl2lgc");
+              })(window, document, "clarity", "script", "r9670un0r8");
               
               var clarity_user_id = localStorage.getItem("clarity_user_id");
               if (!clarity_user_id) {
